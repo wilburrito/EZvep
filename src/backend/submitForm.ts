4896d3b74740343604c63af1aa46a718756d9692
wilburrito <wilbur.fongthemasterchief@gmail.com>
2025-03-13 17:19:38 +0800

    Add API call to backend to post contact form submission to telegram group

--- src/backend/submitForm.ts
+++ src/backend/submitForm.ts
@@ -1,17 +1,30 @@
 import React from 'react';
 import { db } from './firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
+import axios from 'axios';
+
+// const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+// const TELEGRAM_GROUP_ID = process.env.TELEGRAM_GROUP_ID;
+const FIREBASE_ENDPOINT = process.env.REACT_APP_FIREBASE_ENDPOINT || "";
+
 
 export const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, name: string, phoneNumber: string, email: string) => {
     event.preventDefault();
     try {
-        const docRef = await addDoc(collection(db, "contacts"), {
+        await addDoc(collection(db, "contacts"), {
         name: name,
         email: email,
         phoneNumber: phoneNumber,
         timestamp: new Date()
         });
-        console.log("Document written with ID: ", docRef.id);
+        await axios.post(
+            FIREBASE_ENDPOINT,
+            {
+              name,
+              phoneNumber,
+              email,
+            }
+          );
     } catch (e) {
         console.error("Error adding document: ", e);
         alert("An error occurred. Please try again.");
